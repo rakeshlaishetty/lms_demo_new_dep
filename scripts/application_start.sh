396e2b41d7e93d9249ea7cfb79ba17db43201b54
Rakesh laishetty <rakeshlaishetty123@gmail.com>
2022-10-07 17:46:50 +0530

    changes done

---
+++ scripts/application_start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#give permission for everything in the express-app directory
+sudo chmod -R 777 /home/ec2-user/express-app
+
+sudo chmod -R 777 /home
+
+sudo chown -R $(id -u):$(id -g) ~/.npm
+#navigate into our working directory where we have all our github files
+cd /home/ec2-user/express-app
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
+#install node modules
+# npm install
+
+# npm run client
+
+#start our node app in the background
+node index.js > app.out.log 2> app.err.log < /dev/null & 
