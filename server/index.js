dd0ab426a7e1e73b75ad6843ce8d19911699fb4c
Rakesh laishetty <rakeshlaishetty123@gmail.com>
2022-10-08 01:21:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1267,9 +1267,9 @@ app.post("/sendEmail", async (req, res) => {
 });
 
 if (process.env.NODE_ENV !== "production") {
-  app.use(express.static(path.join(__dirname, "../../client/build")));
+  app.use(express.static(path.resolve(__dirname, "client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
   });
 }
 
